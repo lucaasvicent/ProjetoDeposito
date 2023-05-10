0af23138ff9e3906abbbe6153d6b4415f87e8d2a
lucaasvicent <lucasdev08@hotmail.com>
2023-05-10 20:03:08 -0300

    Initial commit

---
+++ src/Application/Program.java
@@ -0,0 +1,52 @@
+package Application;
+
+
+import Entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Account account;
+
+        System.out.print("Enter the account number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter the account holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Is there an initial deposit (y/n)? ");
+        char response = sc.next().charAt(0);
+        if (response == 'y') {
+            System.out.print("Enter the initial deposit value: ");
+            double initialDeposit = sc.nextDouble();
+            account = new Account(number, holder, initialDeposit);
+        }
+        else {
+            account = new Account(number, holder);
+        }
+        System.out.println();
+        System.out.println("Account Data: ");
+        System.out.print(account);
+        System.out.println();
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        double depositValue = sc.nextDouble();
+        account.deposit(depositValue);
+        System.out.println("Updated account data: ");
+        System.out.print(account);
+        System.out.println();
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withdrawValue = sc.nextDouble();
+        account.withdraw(withdrawValue);
+        System.out.println(account);
+
+
+        sc.close();
+    }
+}
